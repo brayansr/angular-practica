2bfd36cd0ed58c3c2e5124a8a5f25e4a59f6dcd9
brayansr <brayansr.basc@gmail.com>
2019-06-30 18:41:22 -0400

    a new commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PersonaComponent } from './persona/persona.component';
+import { PersonasComponent } from './personas/personas.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PersonaComponent,
+    PersonasComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/personas/personas.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personas',
+  templateUrl: './personas.component.html',
+  styleUrls: ['./personas.component.css']
+})
+export class PersonasComponent {
+
+  constructor() { }
+
+
+}
